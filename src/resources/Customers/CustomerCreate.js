1c5ad83dfd1026cdc8a3fdd1dbf4b2babf476e18
patrick <patmagauran.j@gmail.com>
2019-01-31 21:08:17 -0500

    Cleaned up Customers

--- src/resources/Customers/CustomerCreate.js
+++ src/resources/Customers/CustomerCreate.js
@@ -1,28 +1,14 @@
 import {withStyles} from '@material-ui/core';
 import React from 'react';
 
-import {addField, CREATE, Create, fetchUtils, SimpleForm} from 'react-admin';
+import {CREATE, Create, SimpleForm} from 'react-admin';
 import restClient from '../../grailsRestClient';
 import CustomerForm from './CustomerForm';
 
-const httpClient = (url, options = {}) => {
-  if (!options.headers) {
-    options.headers = new Headers({Accept: 'application/json'});
-  }
-  const token = localStorage.getItem('access_token');
-  options.headers.set('Authorization', `Bearer ${token}`);
-  return fetchUtils.fetchJson(url, options);
-};
-const FullForm = addField(({input, meta: {touched, error}, ...props}) => (
-  <CustomerForm {...props}/>
-));
 const dataProvider = restClient;
 const styles = {};
-const reverseGeocode = (address) => {
-  return {lat: 0, long: 0};
-};
+
 const saveCreation = (record, redirect) => {
-  let {lat, long} = reverseGeocode(record.streetAddress + ' ' + record.city + ', ' + record.state + ' ' + record.zipCode);
   let order = record.order;
 
   // order.customer = customer;
@@ -36,8 +22,8 @@ const saveCreation = (record, redirect) => {
       zipCode: record.zipCode,
       phone: record.phone,
       custEmail: record.custEmail,
-      latitude: lat,
-      longitude: long,
+      latitude: 0,
+      longitude: 0,
       ordered: false,
       home: true,
       interested: true,
@@ -51,43 +37,6 @@ const saveCreation = (record, redirect) => {
 
     }
   });
-  /* .then(response => {
-            let customer = {id: response.id};
-            let order = record.order;
-            order.customer = customer;
-            let newOrderedProducts = [];
-            order.orderedProducts.forEach((orderedProduct) => {
-                orderedProduct.customer = customer;
-                newOrderedProducts.push(orderedProduct);
-            });
-            order.orderedProducts = newOrderedProducts;
-            dataProvider(UPDATE, 'Customers', {
-
-                data: {
-                    customerName: record.customerName,
-                    streetAddress: record.streetAddress,
-                    city: record.city,
-                    state: record.state,
-                    zipCode: record.zipCode,
-                    phone: record.phone,
-                    custEmail: record.custEmail,
-                    latitude: lat,
-                    longitude: long,
-                    ordered: false,
-                    home: true,
-                    interested: true,
-                    donation: record.donation,
-                    year: {id: record.year},
-
-                    user: record.user,
-
-                    userName: record.user.userName,
-                    order: order
-
-                }
-
-            });
-        }); */
 };
 
 class CustomerCreate extends React.Component {
@@ -96,78 +45,17 @@ class CustomerCreate extends React.Component {
     this.state = {address: '', zipCode: '', city: '', state: '', update: 0};
   }
 
-    updateAddress = (address) => {
-      let addressObj = {address: '', zipCode: '', city: '', state: '', bldgNum: '', street: ''};
-      for (let i = 0; i < address.address_components.length; i++) {
-        let addressType = address.address_components[i].types[0];
-        let val = address.address_components[i]['short_name'];
+  render () {
+    const {classes, ...props} = this.props;
 
-        switch (addressType) {
-        case 'street_address':
-          addressObj.address = val;
-          break;
-        case 'street_number':
-          addressObj.bldgNum = val;
-
-          break;
-        case 'route':
-          addressObj.street = val;
-
-          break;
-        case 'locality':
-          addressObj.city = val;
-
-          break;
-        case 'administrative_area_level_1':
-          addressObj.state = val;
-
-          break;
-        case 'country':
-
-          break;
-        case 'postal_code':
-          addressObj.zipCode = val;
-
-          break;
-        case 'postal_town':
-          addressObj.city = val;
-
-          break;
-        case 'sublocality_level_1':
-          addressObj.city = val;
-
-          break;
-        }
-      }
-      if (!addressObj.address) {
-        addressObj.address = addressObj.bldgNum + ' ' + addressObj.street;
-      }
-      this.setState({...addressObj, update: 1});
-    };
-
-    render () {
-      const {classes, ...props} = this.props;
-
-      return (
-        <Create title='Create a Customer' {...props}>
-          <SimpleForm save={saveCreation}>
-            <CustomerForm/>
-          </SimpleForm>
-        </Create>
-      );
-    /* <Edit {...props} filters={<CustomerFilter/>}>
-      <ProductsGrid>
-          <TextField label="Customer Name" source="customerName"/>
-          <TextField source="streetAddress"/>
-          <TextField source="city"/>
-          <TextField source="state"/>
-          <NumberField label="Order Cost" source="order.cost" options={{style: 'currency', currency: 'USD'}}/>
-          <NumberField label="Amount Paid" source="order.paid" options={{style: 'currency', currency: 'USD'}}/>
-          <BooleanField label="Delivered?" source="order.delivered"/>
-          <EditButton basePath="/customers"/>
-      </ProductsGrid>
-  </Edit> */
-    }
+    return (
+      <Create title='Create a Customer' {...props}>
+        <SimpleForm save={saveCreation}>
+          <CustomerForm/>
+        </SimpleForm>
+      </Create>
+    );
+  }
 }
 
 export default withStyles(styles)(CustomerCreate);
