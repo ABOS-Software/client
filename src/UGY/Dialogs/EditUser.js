0e81cb40c20687ca7777f258d9018784c4722e5e
patrick <patmagauran.j@gmail.com>
2019-02-03 16:04:53 -0500

    Cleanup Add/Edit User

--- src/UGY/Dialogs/EditUser.js
+++ src/UGY/Dialogs/EditUser.js
@@ -1,93 +1,18 @@
 import React from 'react';
 import {withStyles} from '@material-ui/core';
-import TextField from '@material-ui/core/TextField';
 import PropTypes from 'prop-types';
-import {UPDATE} from 'react-admin';
-import DialogBase from './DialogBase';
 
-import restClient from '../../grailsRestClient';
-
-const dataProvider = restClient;
+import UserActionDialog from './UserActionDialog';
 
 const styles = theme => ({
 
 });
 class EditUser extends React.Component {
-  state = {
-    id: this.props.id,
-    userName: this.props.userName,
-    password: '',
-    fullName: this.props.fullName
-  };
-
-  updateEditUserState = (field, value) => {
-    this.setState({[field]: value});
-  };
-  editUser = event => {
-    dataProvider(UPDATE, 'User', {
-      id: this.state.id,
-      data: {
-        username: this.state.userName,
-        full_name: this.state.fullName,
-
-        password: this.state.password
-      }
-    }).then(response => {
-      this.props.closeDialog();
-    });
-  };
-  renderDialogContent (classes) {
-    return (
-      <div>
-        <TextField
-          value={this.state.userName}
-          label={'Username'}
-          onChange={event => {
-            this.updateEditUserState('userName', event.target.value);
-          }}
-          inputProps={{
-            name: 'UserName',
-            id: 'AddUser-Username'
-          }}
-          disabled
-        />
-        <TextField
-          label={'Password'}
-
-          value={this.state.password}
-          type='password'
-          onChange={event => {
-            this.updateEditUserState('password', event.target.value);
-          }}
-
-          inputProps={{
-            name: 'Password',
-            id: 'AddUser-Password'
-          }}
-        />
-        <TextField
-          label={'FullName'}
-
-          value={this.state.fullName}
-          onChange={event => {
-            this.updateEditUserState('fullName', event.target.value);
-          }}
-
-          inputProps={{
-            name: 'FullName',
-            id: 'AddUser-FullName'
-          }}
-        />
-      </div>
-    );
-  }
   render () {
     const {classes, ...props} = this.props;
-    return (<DialogBase {...props} save={this.editUser}
+    return (<UserActionDialog {...props}
       title={'Edit User'}
-      subText={'Please enter the information about the user'}>
-      {this.renderDialogContent(classes)}
-    </DialogBase>);
+      subText={'Please enter the information about the user'}/>);
   }
 }
 
--- src/UGY/Dialogs/addUser.js
+++ src/UGY/Dialogs/addUser.js
@@ -1,109 +1,20 @@
 import React from 'react';
 import {withStyles} from '@material-ui/core';
-import InputLabel from '@material-ui/core/InputLabel';
-import FormControl from '@material-ui/core/FormControl';
-import TextField from '@material-ui/core/TextField';
-import {CREATE} from 'react-admin';
-
-import restClient from '../../grailsRestClient';
 
 import PropTypes from 'prop-types';
-import DialogBase from './DialogBase';
 
-const dataProvider = restClient;
+import UserActionDialog from './UserActionDialog';
 
 const styles = theme => ({
 
 });
 class addUser extends React.Component {
-  state = {
-    userName: '',
-    password: '',
-    fullName: ''
-  };
-  updateAddUserState = (field, value) => {
-    this.setState({[field]: value});
-  };
-  addSingleUser = event => {
-    dataProvider(CREATE, 'User', {
-      data: {
-        username: this.state.userName,
-        full_name: this.state.fullName,
-        password: this.state.password
-      }
-    }).then(response => {
-      this.props.closeDialog();
-    });
-  };
-
-  renderForm () {
-    const {classes} = this.props;
-
-    return (
-      <div>
-        {this.renderUserName(classes)}
-        {this.renderPassword(classes)}
-        {this.renderFullName(classes)}
-      </div>);
-  }
-
-  renderFullName (classes) {
-    return <FormControl className={classes.formControl}>
-      <InputLabel htmlFor='AddUser-FullName'>FullName</InputLabel>
-      <TextField
-        value={this.state.fullName}
-        onChange={event => {
-          this.updateAddUserState('fullName', event.target.value);
-        }}
-        inputProps={{
-          name: 'FullName',
-          id: 'AddUser-FullName'
-        }}
-      />
-    </FormControl>;
-  }
-
-  renderPassword (classes) {
-    return <FormControl className={classes.formControl}>
-      <InputLabel htmlFor='AddUser-Password'>Password</InputLabel>
-      <TextField
-        value={this.state.password}
-        type='password'
-        onChange={event => {
-          this.updateAddUserState('password', event.target.value);
-        }}
-        inputProps={{
-          name: 'Password',
-          id: 'AddUser-Password'
-        }}
-      />
-    </FormControl>;
-  }
-
-  renderUserName (classes) {
-    return <FormControl className={classes.formControl}>
-      <InputLabel htmlFor='AddUser-Username'>Username</InputLabel>
-      <TextField
-        value={this.state.userName}
-        onChange={event => {
-          this.updateAddUserState('userName', event.target.value);
-        }}
-        inputProps={{
-          name: 'UserName',
-          id: 'AddUser-Username'
-        }}
-      />
-    </FormControl>;
-  }
-
   render () {
     // const {classes} = this.props;
     const {classes, ...props} = this.props;
-    return (<DialogBase {...props} save={this.addSingleUser}
+    return (<UserActionDialog {...props}
       title={'Add User'}
-      subText={'Please enter the information about the user'}>
-      {this.renderForm()}
-    </DialogBase>);
+      subText={'Please enter the information about the user'}/>);
   }
 }
 
