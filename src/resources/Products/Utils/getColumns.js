4fa23d8a60fb54f2479da63881aeff0116e4e06d
patrick <patmagauran.j@gmail.com>
2019-02-03 16:41:52 -0500

    cleaned up GetColumns

--- src/resources/Products/Utils/getColumns.js
+++ src/resources/Products/Utils/getColumns.js
@@ -1,47 +1,71 @@
 import CurrencyFormatter from '../../Formatters/CurrencyFormatter';
 import MUINumberEditor from '../../Editors/MUINumberEditor';
 
+function getHumanProductIdColumn () {
+  return {
+    key: 'humanProductId',
+    name: 'ID',
+    resizable: true
+
+  };
+}
+
+function getProductNameColumn () {
+  return {
+    key: 'productName',
+    name: 'Name',
+    editable: false,
+    resizable: true
+  };
+}
+
+function getSizeColumn () {
+  return {
+    key: 'unitSize',
+    name: 'Size',
+    editable: false,
+    resizable: true
+  };
+}
+
+function getCostColumn () {
+  return {
+    key: 'unitCost',
+    name: 'Unit Cost',
+    editable: false,
+    formatter: CurrencyFormatter,
+    resizable: true
+  };
+}
+
+function getQuantityColumn () {
+  return {
+    key: 'quantity',
+    name: 'Quantity',
+    editable: false,
+    editor: MUINumberEditor,
+    resizable: true
+  };
+}
+
+function getExtendedCostColumn () {
+  return {
+    key: 'extended_cost',
+    name: 'Extended Cost',
+    editable: false,
+    formatter: CurrencyFormatter,
+    resizable: true
+
+  };
+}
+
 export function getColumns () {
   return [
-    {
-      key: 'humanProductId',
-      name: 'ID',
-      resizable: true
-
-    },
-    {
-      key: 'productName',
-      name: 'Name',
-      editable: false,
-      resizable: true
-    },
-    {
-      key: 'unitSize',
-      name: 'Size',
-      editable: false,
-      resizable: true
-    },
-    {
-      key: 'unitCost',
-      name: 'Unit Cost',
-      editable: false,
-      formatter: CurrencyFormatter,
-      resizable: true
-    },
-    {
-      key: 'quantity',
-      name: 'Quantity',
-      editable: false,
-      editor: MUINumberEditor,
-      resizable: true
-    },
-    {
-      key: 'extended_cost',
-      name: 'Extended Cost',
-      editable: false,
-      formatter: CurrencyFormatter,
-      resizable: true
-
-    }
+    getHumanProductIdColumn(),
+    getProductNameColumn(),
+    getSizeColumn(),
+    getCostColumn(),
+    getQuantityColumn(),
+    getExtendedCostColumn()
   ];
 }
