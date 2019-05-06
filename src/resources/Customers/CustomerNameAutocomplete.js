c4d8cd234013cfc096b68acd44319c03b28956dc
patrick <patmagauran.j@gmail.com>
2019-05-05 20:23:33 -0400

    Revert "Cannot get it to accept custom values properly. Should write custom input. ABOS-Software/abos-information#25"
    This reverts commit 8af40ee3

--- src/resources/Customers/CustomerNameAutocomplete.js
+++ src/resources/Customers/CustomerNameAutocomplete.js
@@ -2,19 +2,17 @@ import React from 'react';
 import {withStyles} from '@material-ui/core';
 import AdaptedAutoComplete from './AdaptedAutoComplete';
 import {ReferenceInput} from 'react-admin';
-import PropTypes from 'prop-types';
 
 const styles = () => ({});
 
 class CustomerNameAutocomplete extends React.Component {
   handleSuggestionSelected = (event, {suggestion, method}) => {
-    this.props.updateCustInfo({address: suggestion.streetAddress, city: suggestion.city, state: suggestion.state, zipCode: suggestion.zipCode, phone: suggestion.phone, custEmail: suggestion.custEmail});
+    console.log('test');
   };
   render () {
-    const {updateCustInfo, ...rest} = this.props;
     return (
       <ReferenceInput label='Customer Name' source='customerName' reference='customers'
-        filterToQuery={searchText => ({customer_name: searchText}) } {...rest}
+        filterToQuery={searchText => ({customer_name: searchText})}
       >
 
         <AdaptedAutoComplete handleSelection={this.handleSuggestionSelected}
@@ -28,7 +26,7 @@ class CustomerNameAutocomplete extends React.Component {
 }
 
 CustomerNameAutocomplete.propTypes = {
-  updateCustInfo: PropTypes.func.isRequired
+
 };
 
 export default (withStyles(styles, {withTheme: true})(CustomerNameAutocomplete));
